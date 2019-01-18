db34b70f5461c9945c80b77bf45fe7f53a5b2059
Martin Høgh <mh@mapcentia.com>
2019-01-18 09:57:46 +0100

    Init

---
+++ cmake-build-release/CMakeFiles/ogr2postgis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ogr2postgis.dir/main.cpp.o"
+  "ogr2postgis.pdb"
+  "ogr2postgis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ogr2postgis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/CMakeFiles/ogr2postgis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ogr2postgis.
+# This may be replaced when dependencies are built.
