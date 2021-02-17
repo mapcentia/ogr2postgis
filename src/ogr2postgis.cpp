4102f123bb2978c848499d47625dfca5a4a2a51e
Martin Høgh <mh@mapcentia.com>
2021-02-17 14:10:50 +0100

    Args now implemented

--- src/ogr2postgis.cpp
+++ src/ogr2postgis.cpp
@@ -1,78 +1,62 @@
 #include "ogrsf_frmts.h"
 #include "gdal_utils.h"
 #include <getopt.h>
-#include <unistd.h>
 #include <iostream>
 #include <vector>
 #include <list>
 #include <experimental/filesystem>
-#include <sys/resource.h>
 
 using namespace std;
 
 namespace fs = experimental::filesystem;
-
 
 inline bool caseInsCharCompareN(char a, char b);
 
 bool caseInsCompare(const string &s1, vector<string> s2);
 
 bool translate(const char *pszFilename, const char *encoding, const char *layerName, int featureCount, char *wktString,
-               const char *type);
+               const char *type, char string1[100]);
 
-void start(char const *path);
+void start(char const *path, bool import);
 
-static void help(const char *progname);
+static void help(const char *programName);
+
+const char *connection;
+const char *t_srs{nullptr};
+const char *s_srs{nullptr};
+const char *schema{"public"};
 
 int main(int argc, char *argv[]) {
     int opt;
     int long_index{0};
-    const char *progname{argv[0]};
-    const char *host;
-    const char *port;
-    const char *user;
-    const char *dbname;
-    const char *schema;
-    const char *t_srs;
+    const char *programName{argv[0]};
+    bool import{false};
     const char *path;
 
     if (argc > 1) {
         if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-?") == 0) {
-            help(progname);
+            help(programName);
             exit(0);
         }
     }
 
     //Specifying the expected options
     static struct option long_options[] = {
-            {"host",   required_argument, nullptr, 'h'},
-            {"post",   required_argument, nullptr, 'p'},
-            {"user",   required_argument, nullptr, 'u'},
-            {"dbname", required_argument, nullptr, 'd'},
-            {"schema", required_argument, nullptr, 's'},
-            {"t_srs",  required_argument, nullptr, 't'},
-            {"help",   no_argument,       nullptr, '?'},
-            {nullptr, 0,                  nullptr, 0}
+            {"connection", required_argument, nullptr, 'c'},
+            {"schema",     required_argument, nullptr, 'o'},
+            {"t_srs",      required_argument, nullptr, 't'},
+            {"s_srs",      required_argument, nullptr, 's'},
+            {"import",     no_argument,       nullptr, 'i'},
+            {"help",       no_argument,       nullptr, '?'},
+            {nullptr, 0,                      nullptr, 0}
     };
-    while ((opt = getopt_long(argc, argv, "h:p:U:d:", long_options, &long_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "c:o:t:s:i:", long_options, &long_index)) != -1) {
         switch (opt) {
-            case 'h':
-                printf("host: %s\n", optarg);
-                host = optarg;
+            case 'c':
+                printf("connection: %s\n", optarg);
+                connection = optarg;
                 break;
-            case 'p':
-                printf("port: %s\n", optarg);
-                port = optarg;
-                break;
-            case 'u':
-                printf("user: %s\n", optarg);
-                user = optarg;
-                break;
-            case 'd':
-                printf("dbname: %s\n", optarg);
-                dbname = optarg;
-                break;
-            case 's':
+            case 'o':
                 printf("schema: %s\n", optarg);
                 schema = optarg;
                 break;
@@ -80,10 +64,23 @@ int main(int argc, char *argv[]) {
                 printf("t_srs: %s\n", optarg);
                 t_srs = optarg;
                 break;
+            case 's':
+                printf("s_srs: %s\n", optarg);
+                s_srs = optarg;
+                break;
+            case 'i':
+                printf("import\n");
+                import = true;
+                optind--;
+                break;
             default:
-                help(progname);
+                help(programName);
                 exit(1);
         }
+    }
+    if (argc < 2) {
+        help(programName);
+        exit(1);
     }
 
     // optind is for the extra arguments
@@ -93,135 +90,140 @@ int main(int argc, char *argv[]) {
     }
 
     GDALAllRegister();
-    start(path);
+    start(path, import);
 }
 
-void help(const char *progname) {
-    printf("%s dumps a database as a text file or to other formats.\n\n", progname);
+void help(const char *programName) {
+    printf("%s Iterate recursive through a directory and prints info found geo-spatial vector file formats. Optional import files into to PostGIS\n\n",
+           programName);
     printf("Usage:\n");
-    printf("  %s [OPTION]... [DBNAME]\n", progname);
+    printf("  %s [OPTION]... [DIRECTORY]\n", programName);
 
     printf("\nGeneral options:\n");
-    printf("  -?, --help                   show this help, then exit\n");
+    printf("  -?, --help                    show this help, then exit\n");
 
-    printf("\nOptions controlling the output content:\n");
-    printf("  -a, --data-only              dump only the data, not the schema\n");
+    printf("\nOptions controlling the import to postgis:\n");
+    printf("  -i, --import                  do import into postgis\n");
+    printf("  -o, --schema                  output schema, Defaults to public\n");
+    printf("  -s, --s_srs                   fallback source SRS.\n");
+    printf("  -t, --t_srs                   fallback target SRS. Defaults to EPSG:4326\n");
 
     printf("\nConnection options:\n");
-    printf("  -d, --dbname=DBNAME      database to dump\n");
-    printf("  -h, --host=HOSTNAME      database server host or socket directory\n");
-    printf("  -p, --port=PORT          database server port number\n");
-    printf("  -U, --username=NAME      connect as specified database user\n");
-    printf("  -w, --no-password        never prompt for password\n");
-    printf("  -W, --password           force password prompt (should happen automatically)\n");
+    printf("  -c, --connection=PGDATASOURCE postgres datasource. E.g.\"dbname='databasename' host='addr' port='5432' user='x' password='y'\"\n");
 
-    printf("\nIf no database name is supplied, then the PGDATABASE environment\n"
-           "variable value is used.\n\n");
+//    printf("\nIf no database name is supplied, then the PGDATABASE environment\n"
+//           "variable value is used.\n\n");
 //    printf("Report bugs to <%s>.\n"), PACKAGE_BUGREPORT;
 //    printf("%s home page: <%s>\n"), PACKAGE_NAME, PACKAGE_URL;
 }
 
-void start(const char *path) {
+void start(const char *path, bool import) {
     const int maxFeatures{1};
     string file;
     string fileExtension;
     int countf{0};
     vector<string> extensions{{".tab", ".shp", ".gml", ".geojson", ".json"}};
+    try {
+        for (auto &p: fs::recursive_directory_iterator(path)) {
+            file = p.path().string();
+            fileExtension = p.path().extension();
 
-    for (auto &p: fs::recursive_directory_iterator(path)) {
-        file = p.path().string();
-        fileExtension = p.path().extension();
+            if (caseInsCompare(fileExtension, extensions)) {
+                countf++;
+                GDALDataset *poDS = (GDALDataset *) GDALOpenEx(file.c_str(), GDAL_OF_VECTOR, nullptr, nullptr, nullptr);
+                if (poDS == nullptr) {
+                    printf("Open failed.\n");
+                    exit(1);
+                }
 
-        if (caseInsCompare(fileExtension, extensions)) {
-            countf++;
-            GDALDataset *poDS = (GDALDataset *) GDALOpenEx(file.c_str(), GDAL_OF_VECTOR, nullptr, nullptr, nullptr);
-            if (poDS == nullptr) {
-                printf("Open failed.\n");
-                exit(1);
-            }
-
-            OGRSpatialReference *projection;
-            char *wktString;
-            const char *authorityName;
-            const char *authorityCode;
-            const char *hasWkt{"True"};
-            const char *driverName{poDS->GetDriverName()};
-            char authStr[100];
-            int layerCount{poDS->GetLayerCount()};
-            OGRLayer *layer{poDS->GetLayer(0)};
-            const OGRSpatialReference *reference = layer->GetSpatialRef();
-            if (reference != nullptr) {
-                projection = layer->GetLayerDefn()->OGRFeatureDefn::GetGeomFieldDefn(0)->GetSpatialRef();
-                projection->exportToWkt(&wktString);
-                authorityName = projection->GetAuthorityName(nullptr);
-                authorityCode = projection->GetAuthorityCode(nullptr);
-                if (authorityName != nullptr && authorityCode != nullptr) {
-                    const char *sep{":"};
-                    strcpy(authStr, authorityName);
-                    strcat(authStr, sep);
-                    strcat(authStr, authorityCode);
+                OGRSpatialReference *projection;
+                char *wktString{nullptr};
+                const char *authorityName;
+                const char *authorityCode;
+                const char *hasWkt{"True"};
+                const char *driverName{poDS->GetDriverName()};
+                char authStr[100];
+                int layerCount{poDS->GetLayerCount()};
+                OGRLayer *layer{poDS->GetLayer(0)};
+                const OGRSpatialReference *reference = layer->GetSpatialRef();
+                if (reference != nullptr) {
+                    projection = layer->GetLayerDefn()->OGRFeatureDefn::GetGeomFieldDefn(0)->GetSpatialRef();
+                    projection->exportToWkt(&wktString);
+                    authorityName = projection->GetAuthorityName(nullptr);
+                    authorityCode = projection->GetAuthorityCode(nullptr);
+                    if (authorityName != nullptr && authorityCode != nullptr) {
+                        const char *sep{":"};
+                        strcpy(authStr, authorityName);
+                        strcat(authStr, sep);
+                        strcat(authStr, authorityCode);
+                    } else {
+                        strcpy(authStr, "-");
+                    }
                 } else {
+                    authorityName = "";
+                    authorityCode = "Na";
+                    hasWkt = "False";
                     strcpy(authStr, "-");
                 }
-            } else {
-                authorityName = "";
-                authorityCode = "Na";
-                hasWkt = "False";
-                strcpy(authStr, "-");
-            }
 
-            // Count features
-            GIntBig featureCount = layer->GetFeatureCount(1);
+                // Count features
+                GIntBig featureCount = layer->GetFeatureCount(1);
 
-            int count{0};
-            OGRFeature *poFeature;
-            while ((poFeature = layer->GetNextFeature()) != nullptr) {
-                OGRGeometry *poGeometry = poFeature->GetGeometryRef();
-                const char *type;
-                if (poGeometry != nullptr) {
-                    switch (wkbFlatten(poGeometry->getGeometryType())) {
-                        case 1:
-                            type = "point";
-                            break;
-                        case 2:
-                            type = "linestring";
-                            break;
-                        case 3:
-                            type = "polygon";
-                            break;
-                        case 4:
-                            type = "multipoint";
-                            break;
-                        case 5:
-                            type = "multilinestring";
-                            break;
-                        case 6:
-                            type = "multipolygon";
+                int count{0};
+                OGRFeature *poFeature;
+                while ((poFeature = layer->GetNextFeature()) != nullptr) {
+                    OGRGeometry *poGeometry = poFeature->GetGeometryRef();
+                    const char *type;
+                    if (poGeometry != nullptr) {
+                        switch (wkbFlatten(poGeometry->getGeometryType())) {
+                            case 1:
+                                type = "point";
+                                break;
+                            case 2:
+                                type = "linestring";
+                                break;
+                            case 3:
+                                type = "polygon";
+                                break;
+                            case 4:
+                                type = "multipoint";
+                                break;
+                            case 5:
+                                type = "multilinestring";
+                                break;
+                            case 6:
+                                type = "multipolygon";
+                        }
+                    }
+
+                    printf("%*s %*llu %*s %*i %*s %*s %*s %s", -14, driverName, 8, featureCount, -14, type, 3,
+                           layerCount,
+                           -30, poDS->GetLayer(0)->GetName(), 10, hasWkt, -12, authStr, file.c_str());
+
+                    OGRFeature::DestroyFeature(poFeature);
+
+                    if (import) {
+                        if (translate(file.c_str(), "UTF8", layer->GetName(), featureCount, wktString, type, authStr) ==
+                            FALSE) {
+                            translate(file.c_str(), "LATIN1", layer->GetName(), featureCount, wktString, type, authStr);
+                        };
+                    }
+
+                    count++;
+
+                    if (count == maxFeatures || count == featureCount) {
+                        std::cout << std::endl;
+                        break;
+                    } else {
+                        continue;
                     }
                 }
-
-                printf("%*s %*llu %*s %*i %*s %*s %*s %s", -14, driverName, 8, featureCount, -14, type, 3, layerCount,
-                       -30, poDS->GetLayer(0)->GetName(), 10, hasWkt, -12, authStr, file.c_str());
-
-                OGRFeature::DestroyFeature(poFeature);
-
-                if (1 == 2) {
-                    if (translate(file.c_str(), "UTF8", layer->GetName(), featureCount, wktString, type) == FALSE) {
-                        translate(file.c_str(), "LATIN1", layer->GetName(), featureCount, wktString, type);
-                    };
-                }
-
-                count++;
-
-                if (count == maxFeatures || count == featureCount) {
-                    std::cout << std::endl;
-                    break;
-                } else {
-                    continue;
-                }
+                GDALClose(poDS);
             }
-            GDALClose(poDS);
         }
+    } catch (const std::exception &e) {
+        printf("ERROR: Could not recursive iterate the given directory.\n");
+        exit(1);
     }
     printf("Total %i\n", countf);
     printf("%s\n", GDALVersionInfo("--version"));
@@ -240,7 +242,7 @@ bool caseInsCharCompareN(char a, char b) {
 }
 
 bool translate(const char *pszFilename, const char *encoding, const char *layerName, int featureCount, char *wktString,
-               const char *type) {
+               const char *type, char authStr[100]) {
 
     // Set client encoding with a env
     const char *one{"PGCLIENTENCODING="};
@@ -250,6 +252,12 @@ bool translate(const char *pszFilename, const char *encoding, const char *layerN
     putenv(buf);
 
     char **argv{nullptr};
+    const char *targetSrs = reinterpret_cast<const char *>(wktString != nullptr ? wktString : s_srs);
+    if (targetSrs == nullptr) {
+        std::cout << "   Can't impoort without source srs" << std::endl;
+        CSLDestroy(argv);
+        return TRUE;
+    }
     argv = CSLAddString(argv, "-f");
     argv = CSLAddString(argv, "PostgreSQL");
     argv = CSLAddString(argv, "-lco");
@@ -260,20 +268,22 @@ bool translate(const char *pszFilename, const char *encoding, const char *layerN
     argv = CSLAddString(argv, "PRECISION=NO");
     argv = CSLAddString(argv, "-nlt");
     argv = CSLAddString(argv, type);
-    argv = CSLAddString(argv, "-a_srs"); // Into projection
-    argv = CSLAddString(argv, wktString);
+    argv = CSLAddString(argv, "-s_srs"); // source projection
+    argv = CSLAddString(argv, targetSrs);
     argv = CSLAddString(argv, "-t_srs");
-    argv = CSLAddString(argv, "EPSG:4326"); // Convert to this
+    argv = CSLAddString(argv,
+                        reinterpret_cast<const char *>(strcmp(authStr, "-") != 0 ? authStr : t_srs != nullptr ? t_srs
+                                                                                                              : "EPSG:4326")); // Convert to this
     argv = CSLAddString(argv, "-nln");
 
     char schemaQualifiedName[100];   // array to hold the result.
-    strcpy(schemaQualifiedName, "test."); // copy string one into the result.
+    strcpy(schemaQualifiedName, schema); // copy string one into the result.
+    strcat(schemaQualifiedName, "."); // copy string one into the result.
     strcat(schemaQualifiedName, layerName); // append string two to the result.
 
     argv = CSLAddString(argv, schemaQualifiedName);
-    const char *con = "PG:host=127.0.0.1 user=gc2 password=1234 dbname=dk";
     GDALDatasetH TestDs = GDALOpenEx(pszFilename, GDAL_OF_VECTOR, nullptr, nullptr, nullptr);
-    GDALDatasetH pgDs = GDALOpenEx(con, GDAL_OF_VECTOR,
+    GDALDatasetH pgDs = GDALOpenEx(connection, GDAL_OF_VECTOR,
                                    nullptr, nullptr, nullptr);
     if (pgDs == nullptr) {
         exit(1);
@@ -297,7 +307,7 @@ bool translate(const char *pszFilename, const char *encoding, const char *layerN
             GDALClose(dst);
             return FALSE;
         }
-        std::cout << "Layer translated" << std::endl;
+        std::cout << "   Imported" << std::endl;
         GDALVectorTranslateOptionsFree(opt);
         GDALClose(dst);
         CSLDestroy(argv);
